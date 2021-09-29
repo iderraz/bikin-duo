9d0d6d6166727466c1eabb0234a741506b185b4b
iderraz <iderraz@hotmail.com>
2021-09-29 12:01:40 +0200

    laravelisation

---
+++ resources/views/home.blade.php
@@ -0,0 +1,20 @@
+{{-- @extends('main')
+
+@section('navhero')
+@include('partials.navbar')
+@include('template.section-2')
+
+
+@include('template.aboutUs')
+@include('template.features')
+@include('template.services')
+@include('template.portfolio')
+@include('template.testimonial')
+@include('template.team')
+@include('template.section-2')
+@include('template.contact')
+
+
+@include('partials.footer')
+@include('partials.footerBottom')
+@endsection --}}
